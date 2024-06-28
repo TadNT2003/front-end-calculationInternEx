1906d7f34c193b4c327f3ef5d44279851042297f
Nguyen Thanh Dat <ntdatwows2003@gmail.com>
2024-06-28 16:28:40 +0700

    get solution through API from BE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { Component, useState } from 'react';
+import axios from 'axios'
 
 export default function Home() {
   const [firstPara, setFirstPara] = useState<string>("");
   const [secondPara, setSecondPara] = useState<string>("");
   const [freePara, setFreePara] = useState<string>("");
   const [currentFocus, setCurrentFocus] = useState<number>(0);
+  const [solution, setSolution]  = useState<number[]>([]);
 
   type CalButtonProps = {
     value: string
@@ -68,7 +70,7 @@ export default function Home() {
           <p><input value={firstPara} onFocus={() => {setCurrentFocus(1)}} onChange={(e) => {setFirstPara(e.target.value)}}></input>X<sup>2</sup> 
           + <input value={secondPara} onFocus={() => {setCurrentFocus(2)}} onChange={(e) => {setSecondPara(e.target.value)}}></input>X 
           + <input value={freePara} onFocus={() => {setCurrentFocus(3)}} onChange={(e) => {setFreePara(e.target.value)}}></input> = 0</p>
-          <button onClick={() => {
+          <button onClick={async () => {
             // ExpressionToNumber(firstPara);
             // ExpressionToNumber(secondPara);
             // ExpressionToNumber(freePara);
@@ -77,9 +79,22 @@ export default function Home() {
             }
             else {
               try {
-                console.log(eval(firstPara.replace("√", "Math.sqrt")));
-                console.log(eval(secondPara.replace("√", "Math.sqrt")));
-                console.log(eval(freePara.replace("√", "Math.sqrt")));
+                // console.log(eval(firstPara.replace("√", "Math.sqrt")));
+                // console.log(eval(secondPara.replace("√", "Math.sqrt")));
+                // console.log(eval(freePara.replace("√", "Math.sqrt")));
+                var first = eval(firstPara.replace("√", "Math.sqrt"));
+                var second = eval(secondPara.replace("√", "Math.sqrt"));
+                var free = eval(freePara.replace("√", "Math.sqrt"));
+                const URL = `http://localhost:4000/?firstPara=${first}&secondPara=${second}&freePara=${free}`
+                axios.get(URL)
+                  .then((response) => {
+                    console.log(response.data)
+                    const solution = response.data
+                    setSolution([solution.firstSol, solution.secondSol])
+                  })
+                  .catch((error: any) => {
+                    console.error(error);
+                  })
               }
               catch(e) {
                 alert("Nhập không đúng định dạng")
@@ -94,8 +109,8 @@ export default function Home() {
           }}>Làm mới</button>
         </div>
         <div>
-          <p>X<sub>1</sub>=</p>
-          <p>X<sub>2</sub>=</p>
+          <p>X<sub>1</sub>= {solution[0]}</p>
+          <p>X<sub>2</sub>= {solution[1]}</p>
           <button>Rút gọn kết quả</button>
         </div>
       </div>
